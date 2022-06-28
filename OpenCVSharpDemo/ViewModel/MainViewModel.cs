6806d9dc4a1f83526443b7e3b39bbb79a7b3d40c
rbdev7 <roy.burgess7@outlook.com>
2022-06-29 00:40:02 +0100

    Added Image controls and open file dialog.

---
+++ OpenCVSharpDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System;
+
+namespace OpenCVSharpDemo.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        //[ObservableProperty]
+        string _fileName = "";
+
+        [ICommand]
+        void OpenFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            bool? result = dialog.ShowDialog();
+            if (result != null && result == true)
+            {
+                _fileName = dialog.FileName;
+            }
+        }
+    }
+}

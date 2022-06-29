3e331cda174357bd8c0838255974fb571abef57b
rbdev7 <roy.burgess7@outlook.com>
2022-06-29 01:44:39 +0100

    Added code to convert Bitmap to ImageSource and display via binding.

--- OpenCVSharpDemo/ViewModel/MainViewModel.cs
+++ OpenCVSharpDemo/ViewModel/MainViewModel.cs
@@ -1,7 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace OpenCVSharpDemo.ViewModel
 {
@@ -9,6 +15,17 @@ namespace OpenCVSharpDemo.ViewModel
     {
         //[ObservableProperty]
         string _fileName = "";
+        Mat _img;
+
+        public ImageSource Img 
+        { 
+            get 
+            {
+                if (_img is not null && _img.Width > 0 && _img.Height > 0)
+                    return ConvertBitmapToImageSource(_img.ToBitmap());
+                return null;
+            } 
+        }
 
         [ICommand]
         void OpenFile()
@@ -19,7 +36,27 @@ namespace OpenCVSharpDemo.ViewModel
             if (result != null && result == true)
             {
                 _fileName = dialog.FileName;
+                //_img = new Mat(_fileName);
+                _img = Cv2.ImRead(_fileName);
+                OnPropertyChanged("Img");
             }
+        }
+
+        private ImageSource ConvertBitmapToImageSource(Bitmap imToConvert)
+        {
+            Bitmap bmp = new Bitmap(imToConvert);
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            ImageSource sc = (ImageSource)image;
+
+            return sc;
         }
     }
 }

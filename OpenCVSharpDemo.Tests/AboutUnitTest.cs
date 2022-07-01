cca672a7cd026befde16925c717c4e62549a7307
rbdev7 <roy.burgess7@outlook.com>
2022-07-01 17:46:25 +0100

    Created basic tests.

---
+++ OpenCVSharpDemo.Tests/AboutUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCVSharpDemo.ViewModel;
+//using OpenCVSharpDemo.View;
+using CommunityToolkit.Mvvm;
+
+namespace OpenCVSharpDemo.Tests
+{
+    public class AboutUnitTest
+    {
+        
+        AboutViewModel aboutViewModel;
+        //About about;
+        public AboutUnitTest()
+        { 
+            aboutViewModel = new AboutViewModel();
+            //aboutViewModel.CloseAction = new Action(Window.Close);
+            //about = new About();
+        }
+
+        [Fact]
+        public void PopulateAboutInfoTest()
+        {
+            Assert.True(aboutViewModel.AppInfo.Length > 0);
+            Assert.Contains("Version", aboutViewModel.AppInfo);
+            Assert.Contains("Description", aboutViewModel.AppInfo);
+            Assert.Contains("Author", aboutViewModel.AppInfo);
+        }
+
+        //[Fact]
+        //public void CloseWindowTest()
+        //{
+        //    Assert.True(aboutViewModel.OkCommand.CanExecute(null));
+        //    aboutViewModel.OkCommand.Execute(null);
+        //    Assert.False(aboutViewModel.OkCommand.CanExecute(null));
+        //}
+    }
+}
---
+++ OpenCVSharpDemo.Tests/MainViewModelTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCVSharpDemo.ViewModel;
+using CommunityToolkit.Mvvm;
+using OpenCvSharp;
+
+namespace OpenCVSharpDemo.Tests
+{
+    public class MainViewModelTest
+    {
+        MainViewModel mainViewModel;
+        Mat testMat;
+
+        public MainViewModelTest()
+        {
+            mainViewModel = new MainViewModel();
+            testMat = new Mat(3, 3, MatType.CV_8UC1);
+        }
+
+        [Fact]
+        public void ImgIsNullTest()
+        {
+            Assert.Null(mainViewModel.Img);
+        }
+
+        [Fact]
+        public void ImgWorkingIsNullTest()
+        {
+            Assert.Null(mainViewModel.ImgWorking);
+        }
+
+        [Fact]
+        public void InitialBlurValueTest()
+        {
+            Assert.Equal(1, mainViewModel.BlurValue);
+        }
+
+        //[Fact]
+        //public void BlurCommandTest()
+        //{
+        //    mainViewModel.OpenFileCommand.Execute(null);
+        //}
+
+    }
+}
